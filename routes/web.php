52ac1917a78b145cbc1636e7256ad0f2d64eb761
Diego Pereira <diegoteixeirapereira@hotmail.com>
2021-04-17 02:42:03 -0300

    alteração no arquivo de rotas.

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
+Route::get('/', 'ContatoController@index');
 Route::resource('contatos', ContatoController::class);
